91fd13140241964aea253030c43b02325afc3c52
Ryan Yoo <ryan.yoo@avanade.com>
2019-05-13 13:34:33 -0500

    first commit

---
+++ sum.spec.js
@@ -0,0 +1,28 @@
+const sum = require('./sum.js');
+
+describe('sum suite', function() {
+    test('Should add 2 positive numbers together and return the reuslt', function() {
+        expect(sum(1,2)).toBe(3);
+    });
+
+    test('Should add 2 negative numbers together and return the reuslt', function() {
+        expect(sum(-1,-2)).toBe(-3);
+    });
+
+    test('Should add 1 positive and 1 negative number together and return the reuslt', function() {
+        expect(sum(-1,2)).toBe(1);
+    });
+
+    test('Should add 1 positive and 0 together and return the reuslt', function() {
+        expect(sum(0,2)).toBe(3);
+    });
+
+    test('Should add 1 negative and 0 together and return the reuslt', function() {
+        expect(sum(0,-2)).toBe(-2);
+    });
+
+    test('Should raise an error if one of the inputs is not a number', function() {
+        expect(() => sum("0", -2)).toThrowError('Both arguments must be numbers');
+    });
+});
+ 
